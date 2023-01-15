e732cd251ac533e491bda8a123e48e3b73fda132
Ming-Jung YU <minayu416@gmail.com>
2023-01-15 13:44:34 +0800

    design css by tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function ChatRoom() {
+//   const messagesRef = firestore.collection('messages')
+//   const query = messagesRef.orderBy('createdAt').limit(10);
+
+//   const [messages] = useCollectionData(query, {idField: 'id'})
+// }
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div class='h-96 w-72'>
+        <div class='flex w-72 bg-black opacity-70 rounded-t-3xl'>
+          <div class="mx-3 my-2 bg-cyan-600 rounded-full h-10 w-10"></div>
+            <div>
+            <p class="mt-2 text-sm">CHAT ROOM</p>
+            <p class="text-slate-500 text-xs">Online</p>
+            </div>
+          </div>
+          <ChatRoom />
+
+        </div>
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,10 +36,44 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
 }
+
+
+function ChatRoom(){
+
+    return (
+      <> 
+      <div class='h-96 w-72 bg-black opacity-40 px-4 py-2'>
+            <ChatMessage />
+          </div>
+
+          <div class='flex w-72 bg-black opacity-70 rounded-b-3xl'>
+            <div class="ml-3 my-3 pr-30 text-slate-500 text-xs">Type message...</div>
+            <div class="my-2 px-2 py-1 bg-green-700 rounded-2xl text-xs font-bold">SEND</div>
+          </div>
+      
+      
+      </>
+    )
+}
+
+
+function ChatMessage(){
+
+  return (
+    <>
+      <p>123123</p>
+      <p>123123</p>
+      <p>123123</p>
+    </>
+  )
+
+
+}
+
 
 export default App;
