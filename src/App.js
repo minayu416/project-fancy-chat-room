242e77c66c708f29d279280ce9f73673b3b887f8
Ming-Jung YU <minayu416@gmail.com>
2023-04-01 19:45:03 +0800

    extract .env

--- src/App.js
+++ src/App.js
@@ -13,15 +13,17 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 
 // firebase related code
 const firebaseConfig = {
-  apiKey: "AIzaSyAiuaSJahJeYlOxrz6-TIQ5H1vqu4p1-8o",
-  authDomain: "fancy-chatroom.firebaseapp.com",
-  databaseURL: "https://fancy-chatroom-default-rtdb.firebaseio.com",
-  projectId: "fancy-chatroom",
-  storageBucket: "fancy-chatroom.appspot.com",
-  messagingSenderId: "490120962132",
-  appId: "1:490120962132:web:35ea4e15bfcd527dc84dc1",
-  measurementId: "G-RC7E0JNLHS"
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_UR,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
+console.log(firebaseConfig)
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
